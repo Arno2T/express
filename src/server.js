826c13d21af06755bd7ebeaaf09a920aed5fb82f
Arno <arnaud.tortora@gmail.com>
2018-08-08 15:38:34 +0200

    write route and middleware

--- src/server.js
+++ src/server.js
@@ -1,50 +1,52 @@
 import express from 'express'
 import fs from 'fs'
 
-const app= express()
+const app = express()
 // middlewares
 // ---------------------------------------------------------------------------------
 
-// utiliser la finction use() avant les autres fonctions. Sinon fonction get() exécutées en premier et use() pas exécutée.
-app.use(function(req, res, next){
-    setTimeout(next, 3000)})
-
+// utiliser la fonction use() avant les autres fonctions. Sinon fonction get() exécutées en premier et use() pas exécutée.
+app.use(function(req,res,next){
+    setTimeout(next, 3000)
+})
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8081");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
 })
 
-
-
-// const headerMiddlewares = (req, res, next)=>{
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
-//     next()
-// }
-
-
-
-
 //Routes
-app.get('/ping', (req, res)=>res.send("Pong"))
-// app.get('/ping', (req,res)=>{
-//     res
-//     .status(200)
-//     .setHeader('content-type', 'application/html')
-//     res.send('pong')
-// })
-
-// const jsonData= require('./data/movies.json')
-
-// app.get('/data', (req,res)=> res.send(jsonData))
-
-
-app.get('/data', 
-(req,res)=> {
-    fs.readFile('src/data/movies.json', 'utf8', function (err,data) {
-        res.send(JSON.parse(data));
-})
-   
+let movies
+fs.readFile('src/data/movies.json', 'utf8', function (err, data) {
+    if (err) throw err
+    movies = JSON.parse(data)
 })
 
-app.listen(5000, ()=>console.log('started'))
+app.get('/movie', (req, res) => {
+    res.send(movies.map(movie => {
+        return {
+            id: movie.id,
+            title: movie.title,
+            url: movie.url,
+            autre: movie.autre
+        }
+    }))
+})
+
+app.get('/movie/:id', (req,res)=>{
+  const filteredMovie = movies.find(movie=>{
+        return req.params.id == movie.id
+    })
+    res.send(filteredMovie)
+})
+
+
+
+app.get('/data',
+    (req, res) => {
+        fs.readFile('src/data/movies.json', 'utf8', function (err, data) {
+            res.send(JSON.parse(data));
+        })
+    })
+
+app.listen(5000, () => console.log('started'))
